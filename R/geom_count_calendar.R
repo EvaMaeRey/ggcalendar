96d575c503005a7f88c9ef101d81d3a8edc6bd8f
EvaMaeRey <evangeline.mae@gmail.com>
2022-03-22 17:53:18 -0400

    count and tile

--- R/geom_count_calendar.R
+++ R/geom_count_calendar.R
@@ -1,15 +1,15 @@
-compute_group_calendar <- function(data, scales){
+compute_group_calendar_count <- function(data, scales){
 
   data %>%
-    dplyr::mutate(num_day_of_week = lubridate::wday(.data$date)) %>%
+    dplyr::count(date) %>%
     dplyr::mutate(day_of_week = lubridate::wday(.data$date, label = T, abbr = T)) %>%
     dplyr::mutate(week_of_month = (- lubridate::wday(.data$date) + lubridate::day(.data$date)) %/% 7 + 1) %>%
     dplyr::mutate(date_of_month = lubridate::day(.data$date)) %>%
     dplyr::mutate(which_year = lubridate::year(.data$date) - 2018) %>%
     dplyr::mutate(month = lubridate::month(.data$date, abbr = T, label = T)) %>%
     dplyr::mutate(academic_year =  lubridate::year(.data$date) +
-             ifelse(lubridate::month(date) >
-                      6, 1, 0)) %>%
+                    ifelse(lubridate::month(date) >
+                             6, 1, 0)) %>%
     mutate(academic_month = .data$month %>%
              factor(levels = c("Jul", "Aug", "Sep", "Oct", "Nov", "Dec",
                                "Jan", "Feb", "Mar", "Apr", "May", "Jun"))) %>%
@@ -19,12 +19,13 @@ compute_group_calendar <- function(data, scales){
 
 }
 
-
-StatCalendar <- ggplot2::ggproto(`_class` = "StatCalendar",
+StatCalendarcount <- ggplot2::ggproto(`_class` = "StatCalendarcount",
                                  `_inherit` = ggplot2::Stat,
                                  required_aes = c("date"),
-                                 compute_group = compute_group_calendar,
-                                 default_aes = ggplot2::aes(x = after_stat(x)))
+                                 compute_group = compute_group_calendar_count,
+                                 default_aes = ggplot2::aes(x = after_stat(x),
+                                                            size = after_stat(n))
+                                 )
 
 #' Title
 #'
@@ -40,38 +41,24 @@ StatCalendar <- ggplot2::ggproto(`_class` = "StatCalendar",
 #' @export
 #'
 #' @examples
-#' library(tidyverse)
-#' library(lubridate)
-#'
-#' data.frame(date = as.Date("2020-01-01") + days(0:365)) %>%
-#'   ggplot() +
-#'   aes(date = date) +
-#'   aes(color = date) +
-#'   geom_text_calendar() +
-#'   facet_wrap(~month(date, label = T, abbr = T))
-#'
-#' data.frame(date = as.Date("2020-01-01") + days(0:800)) %>%
-#'   ggplot() +
-#'   aes(date = date) +
-#'   aes(color = date) +
-#'   geom_text_calendar() +
-#'   facet_grid(year(date) ~ month(date, label = T, abbr = T))
-#'
-#'   data.frame(date = as.Date("2020-01-01") + days(0:800)) %>%
-#'   ggplot() +
-#'   aes(date = date) +
-#'   aes(color = date) +
-#'   geom_text_calendar() +
-#'   scale_x_discrete() +
-#'   facet_grid(lubridate::year(date) ~ lubridate::month(date, label = T, abbr = T))
-#'
-geom_text_calendar <- function(mapping = NULL, data = NULL,
+# library(lubridate)
+# library(tidyverse)
+#
+# data.frame(date = as.Date("2020-01-01") + days(0:365) ) %>%
+#   sample_n(500, replace = TRUE) %>%
+#   ggplot() +
+#   aes(date = date) +
+#   geom_count_calendar() +
+#   scale_color_discrete() +
+#   facet_wrap(~month(date, label = T, abbr = T))
+
+geom_count_calendar <- function(mapping = NULL, data = NULL,
                                position = "identity", na.rm = FALSE,
                                show.legend = NA,
                                inherit.aes = TRUE, ...) {
   ggplot2::layer(
-    stat = StatCalendar, # proto object from Step 2
-    geom = ggplot2::GeomText, # inherit other behavior
+    stat = StatCalendarcount, # proto object from Step 2
+    geom = ggplot2::GeomPoint, # inherit other behavior
     data = data,
     mapping = mapping,
     position = position,
--- R/geom_tile_calendar.R
+++ R/geom_tile_calendar.R
@@ -1,4 +1,8 @@
-
+StatCalendartile <- ggplot2::ggproto(`_class` = "StatCalendartile",
+                                 `_inherit` = ggplot2::Stat,
+                                 required_aes = c("date"),
+                                 compute_group = compute_group_calendar,
+                                 default_aes = ggplot2::aes(x = after_stat(num_day_of_week)))
 
 #' Title
 #'
@@ -21,21 +25,21 @@
 #'   ggplot() +
 #'   aes(date = date) +
 #'   aes(color = date) +
-#'   geom_point_calendar() +
+#'   geom_tile_calendar() +
 #'   facet_wrap(~month(date, label = T, abbr = T))
 #'
 #' data.frame(date = as.Date("2020-01-01") + days(0:400)) %>%
 #'   ggplot() +
 #'   aes(date = date) +
-#'   geom_point_calendar() +
+#'   geom_tile_calendar() +
 #'   facet_grid(year(date) ~ month(date, label = T, abbr = T))
-geom_point_calendar <- function(mapping = NULL, data = NULL,
+geom_tile_calendar <- function(mapping = NULL, data = NULL,
                                position = "identity", na.rm = FALSE,
                                show.legend = NA,
                                inherit.aes = TRUE, ...) {
   ggplot2::layer(
-    stat = StatCalendar, # proto object from Step 2
-    geom = ggplot2::GeomPoint, # inherit other behavior
+    stat = StatCalendartile, # proto object from Step 2
+    geom = ggplot2::GeomTile, # inherit other behavior
     data = data,
     mapping = mapping,
     position = position,
